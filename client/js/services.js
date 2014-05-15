cc45e7b2a5e7c28573de3bdf3d12a06840b4f287
Abhishek Dewadiga <adewadiga@mum-1adewadi-m.local>
2014-05-15 16:10:27 +0530

    Added some more blog sources from tumblr

--- client/js/services.js
+++ client/js/services.js
@@ -7,13 +7,22 @@
 //             foo = data;
 //         });
 // }
-
+var tumblr_blogs = [
+	'http://api.tumblr.com/v2/blog/allanimemangaquotes.tumblr.com/posts', //allanimemangaquotes.tumblr.com
+	'http://api.tumblr.com/v2/blog/manga-anime-quotes.tumblr.com/posts', //manga-anime-quotes.tumblr.com
+	'http://api.tumblr.com/v2/blog/anime-quotes-to-live-by.tumblr.com/posts',//http://anime-quotes-to-live-by.tumblr.com
+	'http://api.tumblr.com/v2/blog/animequote.tumblr.com/posts',//http://animequote.tumblr.com
+	'http://api.tumblr.com/v2/blog/quotesfromanime.tumblr.com/posts',//http://quotesfromanime.tumblr.com
+	'http://api.tumblr.com/v2/blog/animesayings-quotes.tumblr.com/posts',//http://animesayings-quotes.tumblr.com
+]
 var quotesReaderServices = angular.module('quotesReaderServices', ['ngResource']);
 
 quotesReaderServices.factory('Posts', ['$resource', '$http',
 	function($resource, $http){
 		// delete $http.defaults.headers.common['X-Requested-With'];
-		return $http.jsonp('http://api.tumblr.com/v2/blog/allanimemangaquotes.tumblr.com/posts',
+
+		var blog_url = tumblr_blogs[5];
+		return $http.jsonp(blog_url ,
 			{
 				params: {
 			        callback: 'JSON_CALLBACK',
@@ -25,6 +34,7 @@ quotesReaderServices.factory('Posts', ['$resource', '$http',
 		        	console.log(data);
 		            console.log('Succesfully fetched the Posts');
 		        });
+
 	}
 ]);
 
